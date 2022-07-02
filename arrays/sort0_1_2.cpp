ff0897ef62aca34c0d049f2c0e426b7e45587004
Deepanshu panwar <99907170+deepanshupanwar8284@users.noreply.github.com>
2022-07-02 17:36:56 +0530

    Create sort0_1_2.cpp

---
+++ arrays/sort0_1_2.cpp
@@ -0,0 +1,21 @@
+/*
+You have been given an integer array/list(ARR) of size 'N'. It only contains 0s, 1s and 2s. Write a solution to sort this array/list.
+
+Try to solve the problem in 'Single Scan'. ' Single Scan' refers to iterating over the array/list just once or to put it in other words
+, you will be visiting each element in the array/list just once.
+
+sample input
+
+2
+6
+0 1 2 2 1 0
+7
+0 1 2 1 2 1 2
+
+sample output
+
+0 0 1 1 2 2
+0 1 1 1 2 2 2
+
+
+*/
