2473fa90e06be1ce0ea91861351cb4ca2328a2ee
Deepanshu panwar <99907170+deepanshupanwar8284@users.noreply.github.com>
2022-07-02 22:57:28 +0530

    Create moveAllNegativeNumbers.cpp

---
+++ arrays/moveAllNegativeNumbers.cpp
@@ -0,0 +1,32 @@
+/*
+You are given an array 'ARR' consisting of 'N' integers. You need to rearrange the array elements such that all negative numbers appear
+before all positive numbers.
+Follow Up:
+Can you solve this in O(1) auxiliary space?
+Note:
+The order of elements in the resulting array is not important.
+Example:
+->Let the array be [1, 2, -3, 4, -4, -5]. On rearranging the array such that all negative numbers appear before all positive numbers 
+we get the resulting array [-3, -5, -4, 2, 4, 1].
+
+*/
+
+// -------------------O(n) approach --------------------
+
+#include <bits/stdc++.h>
+
+vector<int> separateNegativeAndPositive(vector<int> &nums){
+    // Write your code here.
+    vector<int> v;
+    for(int i =0;i<nums.size();i++){
+        if(nums[i]<0){
+            v.push_back(nums[i]);
+        }
+    }
+    for(int i =0;i<nums.size();i++){
+        if(nums[i]>=0){
+            v.push_back(nums[i]);
+        }
+    }
+    return v;
+}
