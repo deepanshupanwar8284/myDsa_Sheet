208f033d4dd64ccae4cb6f2efa6bec375242d43c
Deepanshu panwar <99907170+deepanshupanwar8284@users.noreply.github.com>
2022-07-02 17:28:05 +0530

    Update kthSmallestandLargestElement.cpp

--- arrays/kthSmallestandLargestElement.cpp
+++ arrays/kthSmallestandLargestElement.cpp
@@ -3,6 +3,7 @@
 // Let ‘N’ = 4,  ‘Arr’ be [1, 2, 5, 4] and ‘K’ = 3.  
 // then the elements of this array in ascending order is [1, 2, 4, 5].  Clearly, the 3rd smallest and largest element of this array is 4 and 2 respectively.
 
+// O(n) approach 
 vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
 {
 	// Write your code here.
@@ -11,4 +12,28 @@ vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
     v.push_back(arr[k-1]);
     v.push_back(arr[n-k]);
     return v;
+}
+
+// using priority_queue --- O(logn) approach
+
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>,greater<int>> minHeap; // correct way to create min priority queue (inbuilt max heap hoti h) 
+    for(int i =0;i<n;i++){
+        maxHeap.push(arr[i]);
+        minHeap.push(arr[i]);
+    }
+    for(int i =0;i<k-1;i++){
+        maxHeap.pop();
+        minHeap.pop();
+    }
+    vector<int> v;
+    v.push_back(minHeap.top());
+    v.push_back(maxHeap.top());
+    
+    return v;
+    
+  
+    
 }
