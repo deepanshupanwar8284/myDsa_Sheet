1f640fa7f0a57535d769999197e0659ab0dc4347
Deepanshu panwar <99907170+deepanshupanwar8284@users.noreply.github.com>
2022-07-02 11:04:59 +0530

    Update twoSum.cpp

--- arrays/twoSum.cpp
+++ arrays/twoSum.cpp
@@ -49,3 +49,45 @@ public:
         
     }
 };
+
+// O(1) solution using vector and pair class
+/* in this first we create pair of nums[i] and i and push this pair into new created vector after it 
+   sort the vector and then use two pointer approch 
+   two pointer approch --> keep one pointer at 0th index and second pointer at last indx 
+                           if(target == 0th and last ) 
+                           add pair second value in another new created vector and break their
+                           if( target < sum)
+                           ed--;
+                           else
+                           st++;
+                         */
+#include <bits/stdc++.h>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> v;
+        vector<int> v1;
+        for(int i =0;i<n;i++){
+            pair<int,int> p(nums[i],i);
+            v.push_back(p);
+        }
+        sort(v.begin(),v.end());
+        int st = 0;
+        int ed = v.size()-1;
+        while(st<ed){
+            if(v[st].first+v[ed].first==target){
+                v1.push_back(v[st].second);
+                v1.push_back(v[ed].second);
+                break;
+            }
+            else if(v[st].first+v[ed].first<target){
+                st++;
+            }
+            else{
+                ed--;
+            }
+        }
+        return v1;
+    }
+};
